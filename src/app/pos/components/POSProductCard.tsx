06a82d685d8209a74833f0c326568086b3b9efde
Aliezzat431 <aliezzat02222@gmail.com>
2026-07-11 02:46:51 +0300

    solve POSProductCard.tsx syntax err

--- src/app/pos/components/POSProductCard.tsx
+++ src/app/pos/components/POSProductCard.tsx
@@ -34,7 +34,9 @@ export function POSProductCard({ product, isExpanded, onAddToCart, onToggleBatch
             className={`text-gray-400 transition-transform bg-[#050505]/40 border border-white/10 p-2 rounded-lg hover:text-white ${isExpanded ? 'rotate-180' : ''}`}
             title="عرض التشغيلات المتاحة"
           >
-            <svg xmlns="http:
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <polyline points="6 9 12 15 18 9" />
+            </svg>
           </button>
         </div>
       </div>
